bde7bf1de49dcb2e272fd912a22248951fd53b10
yaeszlo <laszlo@cryptonovae.com>
2022-10-17 12:24:38 +0200

    Add repository action param, Remove project_id action param, Call project details to get id

--- main.js
+++ main.js
@@ -2,15 +2,20 @@ const { getOctokit } = require("@actions/github");
 const core = require("@actions/core");
 
 const token = core.getInput("token");
-const projectId = core.getInput("project_id");
 const columnName = core.getInput("board_done_column_name");
+const repository = core.getInput("repository");
 
+const [owner, repo] = repository.split("/");
 const octokit = getOctokit(token);
 
 async function run() {
   try {
+    const project = await octokit.request("GET /repos/{owner}/{repo}/projects", {
+      owner, repo
+    });
+
     const allColumns = await octokit.request("GET /projects/{project_id}/columns", {
-      project_id: projectId
+      project_id: project.id
     });
 
     console.log(allColumns);
@@ -19,17 +24,17 @@ async function run() {
 
     console.log(`Done column ID=${columnId}`);
 
-    const cards = await octokit.request('GET /projects/columns/{column_id}/cards', {
+    const cards = await octokit.request("GET /projects/columns/{column_id}/cards", {
       column_id: columnId,
       per_page: 100
-    })
+    });
 
     console.log(cards);
 
-    console.log('=======================================');
-    console.log('CONTENT URL');
-    console.log('=======================================');
-    mapToIssues(cards)
+    console.log("=======================================");
+    console.log("CONTENT URL");
+    console.log("=======================================");
+    mapToIssues(cards);
   } catch (e) {
     console.log(e);
     process.exit(1);
@@ -39,7 +44,7 @@ async function run() {
 async function mapToIssues(cards) {
   return cards.map(async card => {
     console.log(`card.content_url`);
-  })
+  });
 }
 
 run();
